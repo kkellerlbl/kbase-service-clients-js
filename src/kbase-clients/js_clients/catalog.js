c39f17a7c34cb2e5d36d8d24dc0c6c53e3c2c9b2
Erik Pearson <eapearson@lbl.gov>
2016-05-26 10:08:02 -0700

    Update to catalog client.

--- src/kbase-clients/js_clients/catalog.js
+++ src/kbase-clients/js_clients/catalog.js
@@ -8,7 +8,7 @@ function Catalog(url, auth, auth_cb, timeout, async_job_check_time_ms, async_ver
 
     this.timeout = timeout;
     var _timeout = timeout;
-    
+
     this.async_job_check_time_ms = async_job_check_time_ms;
     if (!this.async_job_check_time_ms)
         this.async_job_check_time_ms = 5000;
@@ -28,7 +28,7 @@ function Catalog(url, auth, auth_cb, timeout, async_job_check_time_ms, async_ver
         return json_call_ajax("Catalog.version",
             [], 1, _callback, _errorCallback);
     };
- 
+
      this.is_registered = function (params, _callback, _errorCallback) {
         if (typeof params === 'function')
             throw 'Argument params can not be a function';
@@ -41,7 +41,7 @@ function Catalog(url, auth, auth_cb, timeout, async_job_check_time_ms, async_ver
         return json_call_ajax("Catalog.is_registered",
             [params], 1, _callback, _errorCallback);
     };
- 
+
      this.register_repo = function (params, _callback, _errorCallback) {
         if (typeof params === 'function')
             throw 'Argument params can not be a function';
@@ -54,7 +54,7 @@ function Catalog(url, auth, auth_cb, timeout, async_job_check_time_ms, async_ver
         return json_call_ajax("Catalog.register_repo",
             [params], 1, _callback, _errorCallback);
     };
- 
+
      this.push_dev_to_beta = function (params, _callback, _errorCallback) {
         if (typeof params === 'function')
             throw 'Argument params can not be a function';
@@ -67,7 +67,7 @@ function Catalog(url, auth, auth_cb, timeout, async_job_check_time_ms, async_ver
         return json_call_ajax("Catalog.push_dev_to_beta",
             [params], 0, _callback, _errorCallback);
     };
- 
+
      this.request_release = function (params, _callback, _errorCallback) {
         if (typeof params === 'function')
             throw 'Argument params can not be a function';
@@ -80,7 +80,7 @@ function Catalog(url, auth, auth_cb, timeout, async_job_check_time_ms, async_ver
         return json_call_ajax("Catalog.request_release",
             [params], 0, _callback, _errorCallback);
     };
- 
+
      this.list_requested_releases = function (_callback, _errorCallback) {
         if (_callback && typeof _callback !== 'function')
             throw 'Argument _callback must be a function if defined';
@@ -91,7 +91,7 @@ function Catalog(url, auth, auth_cb, timeout, async_job_check_time_ms, async_ver
         return json_call_ajax("Catalog.list_requested_releases",
             [], 1, _callback, _errorCallback);
     };
- 
+
      this.review_release_request = function (review, _callback, _errorCallback) {
         if (typeof review === 'function')
             throw 'Argument review can not be a function';
@@ -104,7 +104,7 @@ function Catalog(url, auth, auth_cb, timeout, async_job_check_time_ms, async_ver
         return json_call_ajax("Catalog.review_release_request",
             [review], 0, _callback, _errorCallback);
     };
- 
+
      this.list_basic_module_info = function (params, _callback, _errorCallback) {
         if (typeof params === 'function')
             throw 'Argument params can not be a function';
@@ -117,7 +117,7 @@ function Catalog(url, auth, auth_cb, timeout, async_job_check_time_ms, async_ver
         return json_call_ajax("Catalog.list_basic_module_info",
             [params], 1, _callback, _errorCallback);
     };
- 
+
      this.add_favorite = function (params, _callback, _errorCallback) {
         if (typeof params === 'function')
             throw 'Argument params can not be a function';
@@ -130,7 +130,7 @@ function Catalog(url, auth, auth_cb, timeout, async_job_check_time_ms, async_ver
         return json_call_ajax("Catalog.add_favorite",
             [params], 0, _callback, _errorCallback);
     };
- 
+
      this.remove_favorite = function (params, _callback, _errorCallback) {
         if (typeof params === 'function')
             throw 'Argument params can not be a function';
@@ -143,7 +143,7 @@ function Catalog(url, auth, auth_cb, timeout, async_job_check_time_ms, async_ver
         return json_call_ajax("Catalog.remove_favorite",
             [params], 0, _callback, _errorCallback);
     };
- 
+
      this.list_favorites = function (username, _callback, _errorCallback) {
         if (typeof username === 'function')
             throw 'Argument username can not be a function';
@@ -156,7 +156,7 @@ function Catalog(url, auth, auth_cb, timeout, async_job_check_time_ms, async_ver
         return json_call_ajax("Catalog.list_favorites",
             [username], 1, _callback, _errorCallback);
     };
- 
+
      this.list_app_favorites = function (item, _callback, _errorCallback) {
         if (typeof item === 'function')
             throw 'Argument item can not be a function';
@@ -169,7 +169,7 @@ function Catalog(url, auth, auth_cb, timeout, async_job_check_time_ms, async_ver
         return json_call_ajax("Catalog.list_app_favorites",
             [item], 1, _callback, _errorCallback);
     };
- 
+
      this.list_favorite_counts = function (params, _callback, _errorCallback) {
         if (typeof params === 'function')
             throw 'Argument params can not be a function';
@@ -182,7 +182,7 @@ function Catalog(url, auth, auth_cb, timeout, async_job_check_time_ms, async_ver
         return json_call_ajax("Catalog.list_favorite_counts",
             [params], 1, _callback, _errorCallback);
     };
- 
+
      this.get_module_info = function (selection, _callback, _errorCallback) {
         if (typeof selection === 'function')
             throw 'Argument selection can not be a function';
@@ -195,7 +195,7 @@ function Catalog(url, auth, auth_cb, timeout, async_job_check_time_ms, async_ver
         return json_call_ajax("Catalog.get_module_info",
             [selection], 1, _callback, _errorCallback);
     };
- 
+
      this.get_version_info = function (params, _callback, _errorCallback) {
         if (typeof params === 'function')
             throw 'Argument params can not be a function';
@@ -208,7 +208,7 @@ function Catalog(url, auth, auth_cb, timeout, async_job_check_time_ms, async_ver
         return json_call_ajax("Catalog.get_version_info",
             [params], 1, _callback, _errorCallback);
     };
- 
+
      this.list_released_module_versions = function (params, _callback, _errorCallback) {
         if (typeof params === 'function')
             throw 'Argument params can not be a function';
@@ -221,7 +221,20 @@ function Catalog(url, auth, auth_cb, timeout, async_job_check_time_ms, async_ver
         return json_call_ajax("Catalog.list_released_module_versions",
             [params], 1, _callback, _errorCallback);
     };
- 
+
+     this.get_module_version = function (selection, _callback, _errorCallback) {
+        if (typeof selection === 'function')
+            throw 'Argument selection can not be a function';
+        if (_callback && typeof _callback !== 'function')
+            throw 'Argument _callback must be a function if defined';
+        if (_errorCallback && typeof _errorCallback !== 'function')
+            throw 'Argument _errorCallback must be a function if defined';
+        if (typeof arguments === 'function' && arguments.length > 1+2)
+            throw 'Too many arguments ('+arguments.length+' instead of '+(1+2)+')';
+        return json_call_ajax("Catalog.get_module_version",
+            [selection], 1, _callback, _errorCallback);
+    };
+
      this.list_local_functions = function (params, _callback, _errorCallback) {
         if (typeof params === 'function')
             throw 'Argument params can not be a function';
@@ -234,7 +247,7 @@ function Catalog(url, auth, auth_cb, timeout, async_job_check_time_ms, async_ver
         return json_call_ajax("Catalog.list_local_functions",
             [params], 1, _callback, _errorCallback);
     };
- 
+
      this.get_local_function_details = function (params, _callback, _errorCallback) {
         if (typeof params === 'function')
             throw 'Argument params can not be a function';
@@ -247,7 +260,7 @@ function Catalog(url, auth, auth_cb, timeout, async_job_check_time_ms, async_ver
         return json_call_ajax("Catalog.get_local_function_details",
             [params], 1, _callback, _errorCallback);
     };
- 
+
      this.module_version_lookup = function (selection, _callback, _errorCallback) {
         if (typeof selection === 'function')
             throw 'Argument selection can not be a function';
@@ -260,7 +273,7 @@ function Catalog(url, auth, auth_cb, timeout, async_job_check_time_ms, async_ver
         return json_call_ajax("Catalog.module_version_lookup",
             [selection], 1, _callback, _errorCallback);
     };
- 
+
      this.list_service_modules = function (filter, _callback, _errorCallback) {
         if (typeof filter === 'function')
             throw 'Argument filter can not be a function';
@@ -273,7 +286,7 @@ function Catalog(url, auth, auth_cb, timeout, async_job_check_time_ms, async_ver
         return json_call_ajax("Catalog.list_service_modules",
             [filter], 1, _callback, _errorCallback);
     };
- 
+
      this.set_registration_state = function (params, _callback, _errorCallback) {
         if (typeof params === 'function')
             throw 'Argument params can not be a function';
@@ -286,7 +299,7 @@ function Catalog(url, auth, auth_cb, timeout, async_job_check_time_ms, async_ver
         return json_call_ajax("Catalog.set_registration_state",
             [params], 0, _callback, _errorCallback);
     };
- 
+
      this.get_module_state = function (params, _callback, _errorCallback) {
         if (typeof params === 'function')
             throw 'Argument params can not be a function';
@@ -299,7 +312,7 @@ function Catalog(url, auth, auth_cb, timeout, async_job_check_time_ms, async_ver
         return json_call_ajax("Catalog.get_module_state",
             [params], 1, _callback, _errorCallback);
     };
- 
+
      this.get_build_log = function (registration_id, _callback, _errorCallback) {
         if (typeof registration_id === 'function')
             throw 'Argument registration_id can not be a function';
@@ -312,7 +325,7 @@ function Catalog(url, auth, auth_cb, timeout, async_job_check_time_ms, async_ver
         return json_call_ajax("Catalog.get_build_log",
             [registration_id], 1, _callback, _errorCallback);
     };
- 
+
      this.get_parsed_build_log = function (params, _callback, _errorCallback) {
         if (typeof params === 'function')
             throw 'Argument params can not be a function';
@@ -325,7 +338,7 @@ function Catalog(url, auth, auth_cb, timeout, async_job_check_time_ms, async_ver
         return json_call_ajax("Catalog.get_parsed_build_log",
             [params], 1, _callback, _errorCallback);
     };
- 
+
      this.list_builds = function (params, _callback, _errorCallback) {
         if (typeof params === 'function')
             throw 'Argument params can not be a function';
@@ -338,7 +351,7 @@ function Catalog(url, auth, auth_cb, timeout, async_job_check_time_ms, async_ver
         return json_call_ajax("Catalog.list_builds",
             [params], 1, _callback, _errorCallback);
     };
- 
+
      this.delete_module = function (params, _callback, _errorCallback) {
         if (typeof params === 'function')
             throw 'Argument params can not be a function';
@@ -351,7 +364,7 @@ function Catalog(url, auth, auth_cb, timeout, async_job_check_time_ms, async_ver
         return json_call_ajax("Catalog.delete_module",
             [params], 0, _callback, _errorCallback);
     };
- 
+
      this.migrate_module_to_new_git_url = function (params, _callback, _errorCallback) {
         if (typeof params === 'function')
             throw 'Argument params can not be a function';
@@ -364,7 +377,7 @@ function Catalog(url, auth, auth_cb, timeout, async_job_check_time_ms, async_ver
         return json_call_ajax("Catalog.migrate_module_to_new_git_url",
             [params], 0, _callback, _errorCallback);
     };
- 
+
      this.set_to_active = function (params, _callback, _errorCallback) {
         if (typeof params === 'function')
             throw 'Argument params can not be a function';
@@ -377,7 +390,7 @@ function Catalog(url, auth, auth_cb, timeout, async_job_check_time_ms, async_ver
         return json_call_ajax("Catalog.set_to_active",
             [params], 0, _callback, _errorCallback);
     };
- 
+
      this.set_to_inactive = function (params, _callback, _errorCallback) {
         if (typeof params === 'function')
             throw 'Argument params can not be a function';
@@ -390,7 +403,7 @@ function Catalog(url, auth, auth_cb, timeout, async_job_check_time_ms, async_ver
         return json_call_ajax("Catalog.set_to_inactive",
             [params], 0, _callback, _errorCallback);
     };
- 
+
      this.is_approved_developer = function (usernames, _callback, _errorCallback) {
         if (typeof usernames === 'function')
             throw 'Argument usernames can not be a function';
@@ -403,7 +416,7 @@ function Catalog(url, auth, auth_cb, timeout, async_job_check_time_ms, async_ver
         return json_call_ajax("Catalog.is_approved_developer",
             [usernames], 1, _callback, _errorCallback);
     };
- 
+
      this.list_approved_developers = function (_callback, _errorCallback) {
         if (_callback && typeof _callback !== 'function')
             throw 'Argument _callback must be a function if defined';
@@ -414,7 +427,7 @@ function Catalog(url, auth, auth_cb, timeout, async_job_check_time_ms, async_ver
         return json_call_ajax("Catalog.list_approved_developers",
             [], 1, _callback, _errorCallback);
     };
- 
+
      this.approve_developer = function (username, _callback, _errorCallback) {
         if (typeof username === 'function')
             throw 'Argument username can not be a function';
@@ -427,7 +440,7 @@ function Catalog(url, auth, auth_cb, timeout, async_job_check_time_ms, async_ver
         return json_call_ajax("Catalog.approve_developer",
             [username], 0, _callback, _errorCallback);
     };
- 
+
      this.revoke_developer = function (username, _callback, _errorCallback) {
         if (typeof username === 'function')
             throw 'Argument username can not be a function';
@@ -440,7 +453,7 @@ function Catalog(url, auth, auth_cb, timeout, async_job_check_time_ms, async_ver
         return json_call_ajax("Catalog.revoke_developer",
             [username], 0, _callback, _errorCallback);
     };
- 
+
      this.log_exec_stats = function (params, _callback, _errorCallback) {
         if (typeof params === 'function')
             throw 'Argument params can not be a function';
@@ -453,7 +466,7 @@ function Catalog(url, auth, auth_cb, timeout, async_job_check_time_ms, async_ver
         return json_call_ajax("Catalog.log_exec_stats",
             [params], 0, _callback, _errorCallback);
     };
- 
+
      this.get_exec_aggr_stats = function (params, _callback, _errorCallback) {
         if (typeof params === 'function')
             throw 'Argument params can not be a function';
@@ -466,7 +479,7 @@ function Catalog(url, auth, auth_cb, timeout, async_job_check_time_ms, async_ver
         return json_call_ajax("Catalog.get_exec_aggr_stats",
             [params], 1, _callback, _errorCallback);
     };
- 
+
      this.get_exec_aggr_table = function (params, _callback, _errorCallback) {
         if (typeof params === 'function')
             throw 'Argument params can not be a function';
@@ -479,7 +492,7 @@ function Catalog(url, auth, auth_cb, timeout, async_job_check_time_ms, async_ver
         return json_call_ajax("Catalog.get_exec_aggr_table",
             [params], 1, _callback, _errorCallback);
     };
- 
+
      this.get_exec_raw_stats = function (params, _callback, _errorCallback) {
         if (typeof params === 'function')
             throw 'Argument params can not be a function';
@@ -492,7 +505,7 @@ function Catalog(url, auth, auth_cb, timeout, async_job_check_time_ms, async_ver
         return json_call_ajax("Catalog.get_exec_raw_stats",
             [params], 1, _callback, _errorCallback);
     };
- 
+
      this.set_client_group = function (group, _callback, _errorCallback) {
         if (typeof group === 'function')
             throw 'Argument group can not be a function';
@@ -505,7 +518,7 @@ function Catalog(url, auth, auth_cb, timeout, async_job_check_time_ms, async_ver
         return json_call_ajax("Catalog.set_client_group",
             [group], 0, _callback, _errorCallback);
     };
- 
+
      this.get_client_groups = function (params, _callback, _errorCallback) {
         if (typeof params === 'function')
             throw 'Argument params can not be a function';
@@ -518,7 +531,7 @@ function Catalog(url, auth, auth_cb, timeout, async_job_check_time_ms, async_ver
         return json_call_ajax("Catalog.get_client_groups",
             [params], 1, _callback, _errorCallback);
     };
- 
+
      this.is_admin = function (username, _callback, _errorCallback) {
         if (typeof username === 'function')
             throw 'Argument username can not be a function';
@@ -531,7 +544,7 @@ function Catalog(url, auth, auth_cb, timeout, async_job_check_time_ms, async_ver
         return json_call_ajax("Catalog.is_admin",
             [username], 1, _callback, _errorCallback);
     };
-  
+
 
     /*
      * JSON call using jQuery method.
@@ -613,5 +626,3 @@ function Catalog(url, auth, auth_cb, timeout, async_job_check_time_ms, async_ver
         return promise;
     }
 }
-
-
